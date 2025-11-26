3fdf1c0ea4bb0781c9b2e9ca15c4c17b564d8641
DiegoDuranBarroso <diduranb@alumnos.unex.es>
2025-11-26 18:36:16 +0100

    cambios

--- src/main/java/com/example/AgroManager_DiegoHugo/controllers/HomeController.java
+++ src/main/java/com/example/AgroManager_DiegoHugo/controllers/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping({"/", "/index"})
+    @GetMapping({"/"})
     public String index() {
         // Carga templates/index.html
         return "index";
